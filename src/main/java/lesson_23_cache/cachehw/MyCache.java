e92836f1d4faa06925e42236c54c3fcaa060fce5
Konstantin Stefankov <stefankov.k@gmail.com>
2026-09-25 13:41:04 +0300

    feat: add chache - core code is from lesson 22 re jpql, - add cache based on WeakHashMap, - programm looks through cache first and if doesn't find value goes to DB, gives value to a listener and put it to cache - like Redis does

--- src/main/java/lesson_23_cache/cachehw/MyCache.java
+++ src/main/java/lesson_23_cache/cachehw/MyCache.java
@@ -1,30 +1,68 @@
 package lesson_23_cache.cachehw;
 
+import java.util.*;
+
 public class MyCache<K, V> implements HwCache<K, V> {
     // Надо реализовать эти методы
+    List<HwListener<K, V>> listeners = new ArrayList<>();
+    Map<K, V> cacheMap = new WeakHashMap<>();
 
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        try {
+            cacheMap.put(key, value);
+            listeners.forEach(l -> l.notify(key, value, "putToCache"));
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
     }
 
     @Override
     public void remove(K key) {
-        throw new UnsupportedOperationException();
+        try {
+            V v = cacheMap.get(key);
+            cacheMap.remove(key);
+            listeners.forEach(l -> l.notify(key, v, "removeFromCache"));
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
     }
 
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        try {
+            V v = cacheMap.get(key);
+            listeners.forEach(l -> l.notify(key, v, "getFromCache"));
+            return v;
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        try {
+            listeners.add(listener);
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        try {
+            listeners.remove(listener);
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    @Override
+    public Map<K, V> getAll() {
+        try {
+            return new HashMap<>(cacheMap);
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
     }
 }
